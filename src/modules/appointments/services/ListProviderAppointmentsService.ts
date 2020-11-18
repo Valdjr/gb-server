f5ae0f1fd9abb6eeeed2013c435972789f872e32
valdljr <valdir.lago@bling.com.br>
2020-11-17 21:13:03 -0300

    feat: add cache provider

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -14,11 +15,17 @@ interface IRequest {
 export default class ListProviderAppointmentsService {
   private appointmentRepository: IAppointmentsRepository;
 
+  private cacheProvider: ICacheProvider;
+
   constructor(
     @inject('AppointmentsRepository')
     appointmentRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    cacheProvider: ICacheProvider,
   ) {
     this.appointmentRepository = appointmentRepository;
+    this.cacheProvider = cacheProvider;
   }
 
   public async execute({
@@ -27,6 +34,10 @@ export default class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+
+    console.log(cacheData);
+
     const appointments = await this.appointmentRepository.findAllInDayFromProvider(
       {
         provider_id,
@@ -35,6 +46,8 @@ export default class ListProviderAppointmentsService {
         year,
       },
     );
+
+    // await this.cacheProvider.save('asd', 'ad');
 
     return appointments;
   }
--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   except_user_id: string;
@@ -11,15 +12,31 @@ interface IRequest {
 export default class ListProvidersService {
   private usersRepository: IUsersRepository;
 
+  private cacheProvider: ICacheProvider;
+
   constructor(
     @inject('UsersRepository')
     usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    cacheProvider: ICacheProvider,
   ) {
     this.usersRepository = usersRepository;
+    this.cacheProvider = cacheProvider;
   }
 
   public async execute({ except_user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders(except_user_id);
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${except_user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders(except_user_id);
+
+      console.log('a query no banco foi feita');
+
+      await this.cacheProvider.save(`providers-list:${except_user_id}`, users);
+    }
 
     return users;
   }
